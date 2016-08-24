f25885f3f8149b403dde816ebb9c9b62d2944762
RuoyuZhang <zry0510@gmail.com>
2016-08-24 16:33:31 -0400

    change folder name

---
+++ copynumber/gwas/prepare/annotate.vcf.sh
@@ -0,0 +1,1 @@
+anno -i /home/fs01/rz253/project/uk10k/twins/vcf/original.vcf/all.chr.vcf.gz -o /home/fs01/rz253/project/uk10k/analysis/copynumber/vcf/all.chr.anno.vcf.gz -r /home/fs01/rz253/bin/anno/resources/hs37d5.fa -g /home/fs01/rz253/bin/anno/resources/refFlat_hg19.txt.gz -p /home/fs01/rz253/bin/anno/resources/priority.txt -c /home/fs01/rz253/bin/anno/resources/codon.txt --indexOutput
