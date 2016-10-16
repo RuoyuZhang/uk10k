86a694075b92971cf5388c12a5d736923ff58526
RuoyuZhang <zry0510@gmail.com>
2016-10-16 09:54:39 -0400

    calculate frequency

--- burden/aggregate.het.R
+++ burden/aggregate.het.R
@@ -29,7 +29,7 @@ for (file in files){
     data=data[data$coverage>100 & data$mutation>opt$min,]
     if (nrow(data)>0){
         #Diversity=apply(data[,c(2,9)],1,Cal_H)
-        Diversity=apply(data[,c(2,9)],1,function(x){x[9]/x[2]})
+        Diversity=apply(data[,c(2,9)],1,function(x){x[2]/x[1]})
         out.sample=cbind(as.character(sample),paste0(data$consensus,data$pos,data$second),Diversity)
         out.table=rbind(out.table,out.sample)
     }else{
